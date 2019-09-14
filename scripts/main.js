6e7fb99b4b0d6b2964feadfc13afc76cf27d77b3
Hans Kristian Smedsrød <eplehans@gmail.com>
2019-09-14 12:16:46 +0200

    fixups

--- scripts/main.js
+++ scripts/main.js
@@ -102,73 +102,7 @@ function init() {
     }).catch(error => {
         console.log(error);
     });
-
-
-    /*fetch("https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@hanserino", {
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        }
-    }).then(function (response) {
-        return response.json().then(function (data) {
-            mediumPosts.data = data;
-            console.log(mediumPosts.data.items);
-
-
-            for (var i = 0; i < mediumPosts.data.items; i++) {
-                console.log(mediumPosts.data.items[i]);
-            }
-
-            mediumPostsEl.dataset.fetchSuccess = true;
-
-
-            for (var key in mediumPosts.data.items) {
-
-                if (mediumPosts.data.items.hasOwnProperty(key)) {
-                    var postPath = mediumPosts.data.posts[key];
-                    var titleEl = "",
-                        subTitleEl = "",
-                        publishedDateEl = "",
-                        thumbEl = "";
-
-                    var publishedDate = moment(postPath.firstPublishedAt).locale('nb').format('Do MMMM YYYY');
-
-                    if (postPath.title) {
-                        titleEl = `<h3 class="medium-article-title">${postPath.title}</h3>`;
-                    }
-                    if (postPath.content.subtitle) {
-                        subTitleEl = `<p class="medium-article-subtitle">${postPath.content.subtitle}</p>`;
-                    }
-                    if (postPath.virtuals.previewImage.imageId) {
-                        thumbEl = `<img class="medium-article-thumb" src="https://cdn-images-1.medium.com/fit/t/200/200/${postPath.virtuals.previewImage.imageId}">`;
-                    }
-                    if (publishedDate) {
-                        publishedDateEl = `<span class="medium-article-date">${publishedDate}</span>`;
-                    }
-
-                    mediumPostsTitleEl.innerHTML = "Siste fra Medium";
-                    mediumPostListContainerEl.innerHTML += `
-                        <li>
-                            <a href="https://medium.com/@hanserino/${postPath.uniqueSlug}">
-                                <div>
-                                    ${publishedDateEl}
-                                    ${thumbEl}
-                                </div>
-                                <div>
-                                    ${titleEl}
-                                    ${subTitleEl}
-                                </div>
-                            </a>
-                        </li>
-                    `;
-                }
-            }
-
-        });
-    }).catch(error => {
-        console.log(error);
-    });
-*/
+    
 
 var mediumPostsEl = document.getElementById('medium-posts-container');
 var mediumPostsTitleEl = document.getElementById('medium-posts-title');
@@ -178,6 +112,7 @@ fetch('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@han
    .then((res) => res.json())
    .then((data) => {
         mediumPostsEl.dataset.fetchSuccess = true;
+        mediumPostsTitleEl.innerHTML = "Skriverier";
 
       // Filter for acctual posts. Comments don't have categories, therefore can filter for items with categories bigger than 0
       const res = data.items //This is an array with the content. No feed, no info about author etc..
